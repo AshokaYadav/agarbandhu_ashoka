9ca1a59234b01e93947242849bb2f8796ca19e47
AshokaYadav <yashoka5111718258@gmail.com>
2024-08-21 04:11:11 +0530

    this is my first commit

---
+++ app/Members/page.js
@@ -0,0 +1,16 @@
+
+import Member from "@/components/Member/Member"
+
+// import Member from "@/components/UpdatedMember/Member"
+
+const page = () => {
+  
+  return (
+    <div>
+      {/* <Member/> */}
+      <Member/>
+    </div>
+  )
+}
+
+export default page
